784d109b5ee4c6cb07a1f2587e53d557f05f94f6
Кечкин Никита Александрович <n.kechkin@rt-solar.ru>
2024-01-17 13:59:32 +0300

    add logic for check_if_patient_is_on_the_list and for exc

--- Hospital.py
+++ Hospital.py
@@ -1,6 +1,13 @@
+from exception import ExceptionNoPatientInHospital
+
+
 class Hospital:
     def __init__(self, list_of_patient):
         self._list_of_patient = list_of_patient
+
+    def check_if_patient_is_on_the_list(self, patient_id):
+        if patient_id > len(self._list_of_patient):
+            raise ExceptionNoPatientInHospital('Ошибка. В больнице нет пациента с таким ID')
 
     def increase_status(self, patient_id):
         self._list_of_patient[patient_id - 1] += 1
---
+++ exception.py
@@ -0,0 +1,10 @@
+class ExceptionNoPatientInHospital(Exception):
+    def __init__(self, *args):
+        if args:
+            self.message = args[0]
+        else:
+            self.message = None
+
+    def __str__(self):
+        if self.message:
+            return self.message
