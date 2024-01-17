fb18584565f47c19bc80652dcd17b69cb0586bea
Кечкин Никита Александрович <n.kechkin@rt-solar.ru>
2024-01-17 12:35:52 +0300

    add test all statuses

--- tests/test_get_patient_status.py
+++ tests/test_get_patient_status.py
@@ -4,3 +4,12 @@ from Hospital import Hospital
 def test_get_status_name_by_patient_id():
     entity = Hospital([1, 0, 1])
     assert entity.get_status_name_by_patient_id(patient_id=2) == 'Тяжело болен'
+
+
+def test_get_all_statuses_name_by_patient_id():
+    entity = Hospital([0, 1, 2, 3])
+    assert entity.get_status_name_by_patient_id(patient_id=1) == 'Тяжело болен'
+    assert entity.get_status_name_by_patient_id(patient_id=2) == 'Болен'
+    assert entity.get_status_name_by_patient_id(patient_id=3) == 'Слегка болен'
+    assert entity.get_status_name_by_patient_id(patient_id=4) == 'Готов к выписке'
+
